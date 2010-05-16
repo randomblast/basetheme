da7ee0f7e39103df27600851dfd6c6ea50ad9b69
josh <josh@li184-135.members.linode.com>
2010-05-16 16:18:03 -0400

    Added soem basic templates with header & footer sidebars

--- functions.php
+++ functions.php
@@ -1,4 +1,13 @@
 <?
 	// This file must be included for Themebase to work properly
 	require_once('.build/generated.php');
+
+	register_sidebar(array(
+		  'name' => 'Header'
+		, 'id' => 'sidebar-header'
+	));
+	register_sidebar(array(
+		  'name' => 'Footer'
+		, 'id' => 'sidebar-footer'
+	));
 ?>
